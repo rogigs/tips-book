a2f20cdd102d8dd81727847b1d6a01a46deb63de
Igor Soares <igorgomes.13ig@gmail.com>
2023-06-17 01:21:28 -0300

    feat: add control button to follow and unfollow

--- src/pages/User/index.js
+++ src/pages/User/index.js
@@ -2,7 +2,7 @@ import { StyleSheet, View } from "react-native";
 import { Avatar, Button, Text } from "react-native-paper";
 import { LinearGradient } from "expo-linear-gradient";
 import { useEffect, useState } from "react";
-import { ref, onValue, set, push } from "firebase/database";
+import { ref, onValue, set } from "firebase/database";
 import { WrapperScreenTabs } from "../../components/WrapperScreenTabs";
 import { COLORS } from "../../assets/styles/colors";
 import { Card } from "../../components/Card";
@@ -38,27 +38,51 @@ export default function User({ navigation, route }) {
   const [posts, setPosts] = useState({});
   const [following, setFollowing] = useState({});
   const [followers, setFollowers] = useState({});
+  const [isAFollower, setIsAFollower] = useState(false);
 
   const openPost = () => navigation.navigate("Post");
-  const { state, dispatch } = useUser();
+  const { state } = useUser();
 
-  const ownerProfile = route.params?.userIdOfProfileVisited ?? state.userId;
+  const userIdOfProfileVisited = route.params?.userIdOfProfileVisited;
+  const ownerProfile = userIdOfProfileVisited ?? state.userId;
 
-  const followUser = () => {
+  const followUser = ({ followUser }) => {
     const postListRef = ref(database, "follows");
     set(postListRef, {
       [state.userId]: {
         following: {
-          [ownerProfile]: true,
+          [ownerProfile]: followUser,
         },
       },
       [ownerProfile]: {
         follower: {
-          [state.userId]: true,
+          [state.userId]: followUser,
         },
       },
     });
+
+    setIsAFollower(followUser);
   };
+
+  const buttonProperties = userIdOfProfileVisited
+    ? {
+        name: isAFollower ? "Deixar de seguir" : "Seguir",
+        screenToGo: "",
+        icon: isAFollower ? "account-minus" : "account-plus",
+        color: isAFollower ? COLORS.LIGHT : COLORS.SECONDARY,
+        textColor: isAFollower ? COLORS.SECONDARY : COLORS.LIGHT,
+        onPress: isAFollower
+          ? () => followUser({ followUser: false })
+          : () => followUser({ followUser: true }),
+      }
+    : {
+        name: "Editar",
+        screenToGo: "Edit",
+        icon: "tools",
+        color: COLORS.SECONDARY,
+        textColor: COLORS.LIGHT,
+        onPress: () => {},
+      };
 
   useEffect(() => {
     const starCountRef = ref(database, `post/${ownerProfile}`);
@@ -74,16 +98,23 @@ export default function User({ navigation, route }) {
 
   useEffect(() => {
     const starCountRef = ref(database, `follows/${ownerProfile}`);
+
     onValue(starCountRef, (snapshot) => {
       try {
         const data = snapshot.val();
-        setFollowers(Object.keys(data.follower || {}));
-        setFollowing(Object.keys(data.following || {}));
+
+        setFollowers(Object.keys(data?.follower || {}));
+        setFollowing(Object.keys(data?.following || {}));
+        setIsAFollower(
+          Object.entries(data.follower).some(
+            (follower) => follower[0] === state.userId && follower[1] === true
+          )
+        );
       } catch (error) {
         console.error(error);
       }
     });
-  }, []);
+  }, [isAFollower]);
 
   return (
     <WrapperScreenTabs openPost={openPost}>
@@ -103,11 +134,12 @@ export default function User({ navigation, route }) {
           <Button
             mode="elevated"
             size={32}
-            onPress={followUser}
-            buttonColor={COLORS.SECONDARY}
-            textColor={COLORS.LIGHT}
+            onPress={buttonProperties.onPress}
+            buttonColor={buttonProperties.color}
+            textColor={buttonProperties.textColor}
+            icon={buttonProperties.icon}
           >
-            Seguir
+            {buttonProperties.name}
           </Button>
         </View>
       </LinearGradient>
