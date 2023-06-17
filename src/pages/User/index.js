505d4f2b1524e3cbd3a710525f0e5d16e9eded92
Igor Soares <igorgomes.13ig@gmail.com>
2023-06-16 22:11:30 -0300

    feat: add method of following

--- src/pages/User/index.js
+++ src/pages/User/index.js
@@ -2,11 +2,12 @@ import { StyleSheet, View } from "react-native";
 import { Avatar, Button, Text } from "react-native-paper";
 import { LinearGradient } from "expo-linear-gradient";
 import { useEffect, useState } from "react";
-import { ref, onValue } from "firebase/database";
+import { ref, onValue, set, push } from "firebase/database";
 import { WrapperScreenTabs } from "../../components/WrapperScreenTabs";
 import { COLORS } from "../../assets/styles/colors";
 import { Card } from "../../components/Card";
 import { database } from "../../../firebaseConfig";
+import { useUser } from "../../context/useUser";
 
 const styles = StyleSheet.create({
   container: {
@@ -33,16 +34,47 @@ const styles = StyleSheet.create({
   },
 });
 
-export default function User({ navigation }) {
+export default function User({ navigation, route }) {
   const [posts, setPosts] = useState({});
+  const [followers, setFollowers] = useState([]);
+
   const openPost = () => navigation.navigate("Post");
+  const { state } = useUser();
+
+  const idProfileVisited = route.params?.isProfilelVisited;
+  const userId = idProfileVisited ?? state.userId;
+  console.log("🚀 ~ file: index.js:47 ~ User ~ userId:", userId);
+
+  const followUser = () => {
+    const postListRef = ref(database, "follows");
+    set(postListRef, {
+      [userId]: {
+        following: {
+          [userId]: true,
+        },
+      },
+      // outro seguidor
+    });
+  };
 
   useEffect(() => {
-    const starCountRef = ref(database, "post/123");
+    const starCountRef = ref(database, `post/${userId}`);
     onValue(starCountRef, (snapshot) => {
       try {
         const data = snapshot.val();
         setPosts(data);
+      } catch (error) {
+        console.error(error);
+      }
+    });
+  }, []);
+
+  useEffect(() => {
+    const starCountRef = ref(database, `follows/${userId}`);
+    onValue(starCountRef, (snapshot) => {
+      try {
+        const data = snapshot.val();
+        setFollowers(Object.keys(data.following));
       } catch (error) {
         console.error(error);
       }
@@ -59,24 +91,31 @@ export default function User({ navigation }) {
         <Text style={styles.title}>Username</Text>
         <Text>@username</Text>
         <View style={styles.containerButtonsToFollow}>
+          <Text>Seguidores {followers.length}</Text>
+          <Text>| </Text>
+          <Text>Seguindo</Text>
+        </View>
+        <View style={styles.containerButtonsToFollow}>
           <Button
             mode="elevated"
             size={32}
-            onPress={() => {}}
+            onPress={followUser}
             buttonColor={COLORS.SECONDARY}
             textColor={COLORS.LIGHT}
           >
             Seguir
           </Button>
-          <Button
-            mode="elevated"
-            size={32}
-            onPress={() => {}}
-            buttonColor={COLORS.SECONDARY}
-            textColor={COLORS.LIGHT}
-          >
-            Editar
-          </Button>
+          {!idProfileVisited && (
+            <Button
+              mode="elevated"
+              size={32}
+              onPress={() => {}}
+              buttonColor={COLORS.SECONDARY}
+              textColor={COLORS.LIGHT}
+            >
+              Editar
+            </Button>
+          )}
         </View>
       </LinearGradient>
       <View>
