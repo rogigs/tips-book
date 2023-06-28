fe914a4a31e335b9b1ded32f63ea106b744a7764
Igor Soares <igorgomes.13ig@gmail.com>
2023-06-28 12:16:29 -0300

    feat: add screens web

--- src/pages/Auth/web/index.js
+++ src/pages/Auth/web/index.js
@@ -1,33 +1,146 @@
 import { NavigationContainer } from '@react-navigation/native';
-import { Text } from 'react-native-paper'
-import { createNativeStackNavigator } from "@react-navigation/native-stack";
-import Login from '../../Login';
+import { createStackNavigator } from '@react-navigation/stack';
 import Home from '../../Home';
+import { useUser } from '../../../context/useUser';
+import Login from '../../Login';
+import User from "../../User";
+import Post from "../../Post";
+import CreateAccount from '../../CreateAccount';
+import EditAccount from '../../EditAccount';
+import { createMaterialBottomTabNavigator } from "@react-navigation/material-bottom-tabs";
+import { COLORS } from '../../../assets/styles/colors';
+import { StyleSheet } from "react-native";
+
+import { Button } from "react-native-paper";
+
+const Stack = createStackNavigator();
 
 const config = {
+  initialRouteName: 'Login',
   screens: {
-    Home: '/',
-    Login: 'login',
+    Login: '/',
   },
 };
 
 const linking = {
-  prefixes: ['http://localhost:19008/', 'localhost:19008://'],
   config,
 };
 
-const Stack = createNativeStackNavigator();
+const Tab = createMaterialBottomTabNavigator();
 
-export function Auth() {
+
+const styles = StyleSheet.create({
+  tabNavigator: {
+    backgroundColor: COLORS.SECONDARY,
+  },
+});
+
+function HomeScreen() {
   return (
-    <NavigationContainer linking={linking} fallback={<Text>Loading...</Text>}>
-         <Stack.Navigator>
-        <Stack.Screen name="Home" component={Home} />
-        <Stack.Screen name="Login" component={Login} />
-      </Stack.Navigator>
-    </NavigationContainer>
+    <Stack.Navigator initialRouteName="Feed">
+      <Stack.Screen
+        name="Feed"
+        component={Home}
+        options={{ headerShown: false }}
+      />
+      <Stack.Screen
+        name="UserVisited"
+        component={User}
+        options={{ title: "" }}
+      />
+    </Stack.Navigator>
+  );
+}
+
+function PostScreen() {
+  return (
+    <Stack.Navigator initialRouteName="Account">
+      <Stack.Screen
+        name="Account"
+        component={User}
+        options={{ headerShown: false }}
+      />
+      <Stack.Screen
+        name="EditAccount"
+        component={EditAccount}
+        options={{ title: "Editar" }}
+      />
+    </Stack.Navigator>
+  );
+}
+
+function WithTab() {
+  return (
+    <Tab.Navigator
+      initialRouteName="Home"
+      style={styles.tabNavigator}
+      barStyle={{ backgroundColor: COLORS.PRIMARY, color: COLORS.LIGHT }}
+      inactiveColor={COLORS.SECONDARY}
+      activeColor={COLORS.LIGHT}
+      tabBarActiveBackgroundColor={COLORS.LIGHT}
+    >
+      <Tab.Screen
+        name="Home"
+        component={HomeScreen}
+        options={{
+          tabBarIcon: ({ color, size }) => (
+            <Button icon="home-account" textColor={color} size={size} />
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="User"
+        component={PostScreen}
+        options={{
+          tabBarIcon: ({ color, size }) => (
+            <Button icon="account" textColor={color} size={size} />
+          ),
+        }}
+      />
+    </Tab.Navigator>
   );
 }
 
 
 
+export function Auth() {
+  const { state } = useUser();
+
+  const authenticationUser = state.tokenId;
+
+  return (
+    <NavigationContainer linking={linking}>
+
+      <Stack.Navigator>
+        {authenticationUser ? (
+          <>
+            <Stack.Screen
+              name="Tabs"
+              component={WithTab}
+              options={{ headerShown: false }}
+            />
+            <Stack.Screen name="Post" component={Post} options={{ title: "" }} />
+          </>
+        ) : (
+          <>
+            <Stack.Screen
+              name="Login"
+              component={Login}
+              options={{ headerShown: false }}
+            />
+            <Stack.Screen
+              name="CreateAccount"
+              component={CreateAccount}
+              options={{ headerShown: false }}
+            />
+            <Stack.Screen
+              name="EditAccount"
+              component={EditAccount}
+              options={{ headerShown: false }}
+            />
+          </>
+        )}
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
