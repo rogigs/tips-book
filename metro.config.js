c93a2f18b55719ae328404d3a533d795dfa9bd43
Igor Soares <igorgomes.13ig@gmail.com>
2023-07-10 20:32:45 -0300

    chore: add rules of firebase's deploy

--- metro.config.js
+++ metro.config.js
@@ -1,6 +1,8 @@
 const { getDefaultConfig } = require("@expo/metro-config");
 
 const defaultConfig = getDefaultConfig(__dirname);
+
 defaultConfig.resolver.assetExts.push("cjs");
+defaultConfig.transformer.minifierPath = 'metro-minify-uglify';
 
 module.exports = defaultConfig;
